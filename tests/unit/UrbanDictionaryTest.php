6de66b1676664869b42e7d6d30cee8b30de8636f
Alex Mills <alex@asdfx.us>
2019-02-05 16:18:35 +0000

    added phpunit xml and test class

---
+++ tests/unit/UrbanDictionaryTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class UrbanDictionaryTest extends TestCase {
+
+    /**
+     * @test
+     */
+    public function it_can_request_wat_from_urban_dictionary()
+    {
+        $urbanDictionary = new \Asdfx\UrbanDictionary\UrbanDictionary('ca3a830972msh867fa6eeeb7bf66p1dc977jsndfe95b69fedc');
+        $result = $urbanDictionary->lookup('wat');
+        die(var_dump(result));
+    }
+}
